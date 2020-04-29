89733791e0b7209f50a5f6de54b938a65002c203
victor2190 <viescalo@umag.cl>
2020-04-28 22:20:37 -0300

    Commit 13. sigo el proyecto fuera de evaluacion...proyecto evaluado en commit 12

---
+++ app/src/main/java/com/example/repartos2020/modelo/ListaEncomiendas.java
@@ -0,0 +1,8 @@
+package com.example.repartos2020.modelo;
+
+import java.util.ArrayList;
+
+public class ListaEncomiendas {
+    private static ListaEncomiendas instancia=new ListaEncomiendas();
+    private ArrayList<Encomienda> listaEncomiendas;
+}
